52272b864d42a4f6c6cf126ad38ce3f4a0aaa067
Ziyad Noorhassan <67880504+thEchroniCamateuR@users.noreply.github.com>
2024-05-01 18:32:12 -0400

    Add files via upload

--- javascriptsyntax.js
+++ javascriptsyntax.js
@@ -197,9 +197,18 @@ functionName(arguements)
 
 //Objects
 
+//plain objects are the most known
+
+//object literals: creating anew object with properties,which are key:value pairs
+
 let exampleObject = {
     property1 = "this is the first property",
     property2 = "another one",
+    "multi-word-property": true,
+    propertyObject: {
+        subpropertyone = "nested!"
+    },
+
     explainMethod: function ExplainFunction() {
         action;
         action;
@@ -214,4 +223,112 @@ let exampleObject = {
 }
 
 exampleObject.property1 //value of property1 inside object
+exampleObject['multi-word-property']; //value of  multi-word-property
 exampleObject.explainMethod() //returns value of the function inside object
+exampleObject.propertyObject; //returns ????
+exampleObject.propertyObject.subpropertyone; //returns nested property from property obejct
+
+
+//object constructors can be used to instansiate new objects of functions or create empty objects
+
+let exampleObjectTwo = new Object(); //empty obj
+
+function User(name){
+    let name = name;
+    return name;
+
+}
+
+let playerOne = new User("Bob"); //object from function
+let playerTwo = new User("Bill"); //another object from same function
+
+
+
+
+//delete properties
+
+delete exampleObject.property1; 
+
+//"in" tests if property in object
+
+"key" in onject_name //value will be true or false
+
+//"for..in" loop
+/*(?) for (let key in object_name){
+    action;
+}*/
+
+
+for (key in object_name){
+
+    action;
+}
+
+//int keys  are ordered from least to most, all other values are ordered according to creation order
+
+//copying objects just creates references to the same object (unlike primitives);
+//to duplicate the literal object, either iterate over properties and put them in a new object via construuctor
+//or use Object.assign - Object.assign(destination, ...sources);
+
+
+//creates original object
+let originalObject = {
+
+    attribute: 1
+    
+}
+
+
+
+let copiedreference = originalObject; //merely references object (by memory)
+
+let secondObject = new Object(); //constructs empty object
+
+
+//assigns properties from one object to the other, therby cloning the original object. They will be the same object but occupy two different memory addresses
+for (key in originalObject){
+    Object.assign(secondObject, originalObject)
+}
+
+//look into structuredClone method to clone properties that are also objects
+
+//"this"
+
+let test = {
+
+    someProperty = 1,
+    someProperty2 = 2,
+    someProperty3 = 3,
+
+    dialogue() {
+        let inner = (this.someProperty3); //"this" references  property of outer nest of nested functions
+
+    }
+}
+
+
+//symbols..look into symbols. Unique identifiers for objects. Unique properties
+//also look into Object to Primative conversion
+
+
+//Data Types and methods/properties
+//Data types can have either methods (with ()) or properties (without ()) to change the data.
+
+//Arrays - a type of object  ordered collections of data,indexed starting at 0
+
+let nameOfArray = new Array(); // Array constructor
+
+let nameOfAnotherArray = [] //Array literal
+
+//for..of loops work for iterating over arrays
+
+
+// Maps are also objects(?), but allow for {key:value} pairs with keys of any type (as opposed to objects which just allow only strings and symbols as keys)
+
+let nameName = new Map(); //map constructor
+
+//Sets are like Maps but only allow for values to appear only once
+//WeakMaps are like Maps but only objects can be keys
+//WeakSets are like Sets but only objects can be keys
+//Array and Object destructuring = only interate over certain parts of the data 
+//JSONS are a thing...
